26e8671c9495a36cb97c142b837de4bf182b56d1
Thomas Porter <thomasporter2019@gmail.com>
2020-08-10 16:07:20 -0400

    Update handle_run.py

--- config_generator_gui/cgi-bin/handle_run.py
+++ config_generator_gui/cgi-bin/handle_run.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+# © Copyright 2020, D-Tech, LLC, All Rights Reserved. 
+# Version: 0.5 (initial version), 08/10/2020
+# License: The use of this software program is subject to the ML-ExLab 
+# license terms and conditions as defined in the LICENSE file.
+# Disclaimer: This software is provided "AS IS" without warrantees.  
+# D-Tech, LLC has no obligation to provide any maintenence, update 
+# or support for this software.  Under no circumstances shall D-Tech,  
+# LLC be liable to any parties for direct, indirect, special, incidental,
+# or consequential damages, arising out of the use of this software
+# and related data and documentation.
+#
+
 import cgi, cgitb 
 
 form = cgi.FieldStorage()
